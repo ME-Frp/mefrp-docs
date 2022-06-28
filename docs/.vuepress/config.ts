3fe94bb11497cc1497bb35fcb36886c8f147f075
admin <admin@mcserverx.top>
2022-06-28 09:50:24 +0800

    首次提交

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'ME Frp 文档',
+    themeConfig: {
+      nav: [
+        { text: 'ME Frp手册', link: '/' },
+        { text: 'ME Frp官网', link: 'https://mefrp.cn' },
+        { text: 'MCServerX Network', link: 'https://www.mcserverx.com' },
+      ],
+      sidebar: [
+        ['/manual/understand', '开始之前'],
+        ['/manual/system', '选择和下载启动器'],
+        ['/manual/Windows', 'Windows 系统'],
+        ['/manual/Linux', 'Linux 系统']
+      ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroText: ME Frp官方手册
+tagline: null
+actionText: 快速上手 →
+actionLink: /manual/understand.html
+footer: MIT Licensed | Copyright © 2022 ME Frp.
+---
